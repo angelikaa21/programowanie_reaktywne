66b362a7ad99b5f0d8a453b501f6aba6563727b9
Angelika Król <andziua.krl@gmailcom>
2023-06-10 22:03:29 +0200

    Lab9

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import NotFound from './components/notFound';
+import Home from "./components/home";
+import Posts from "./components/posts";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App/>}>
+          <Route path="home" element={<Home/>}/>
+          <Route path="posts" element={<Posts/>}/>
+          <Route path="*" element={<NotFound/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>  
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
